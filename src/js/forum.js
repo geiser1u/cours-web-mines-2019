625a1ed548b2f839b86d29ee34137d586f5f3b95
root <root@Sylvain-GEISER.localdomain>
2019-11-19 10:12:54 +0100

    Done

--- src/js/forum.js
+++ src/js/forum.js
@@ -1,22 +1,24 @@
 import ky from "ky";
-
-async function refreshMessages() {
-  // GET https://ensmn.herokuapp.com/messages
-  const messages = await ky.get("https://ensmn.herokuapp.com/messages").json();
-  console.log(messages[0].author);
-  displayMessages(messages);
-}
-
-function displayMessages(messages) {
-  // Clear list content on view
-  // Iterate on messages and display getMessageView(message);
-}
+import $ from "jquery";
 
 function getMessageView(message) {
   return `<div>
     <p>${message.username}<p/>
     <p>${message.message}</p>
   </div>`;
+}
+
+function displayMessages(messages) {
+  // Clear list content on view
+  $(".messages-container").empty();
+  // Iterate on messages and display getMessageView(message);
+}
+
+async function refreshMessages() {
+  // GET https://ensmn.herokuapp.com/messages
+  const messages = await ky.get("https://ensmn.herokuapp.com/messages").json();
+  console.log(messages[0].author);
+  displayMessages(messages);
 }
 
 setInterval(() => {
