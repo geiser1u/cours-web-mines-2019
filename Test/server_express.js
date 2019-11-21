93a0b67aeb7121710d0bce35a0be16089919d769
geiser1u <sylvain.geiser2@etu.univ-lorraine.fr>
2019-11-21 16:06:36 +0100

    Done

--- Test/server_express.js
+++ Test/server_express.js
@@ -3,9 +3,20 @@ const Express = require('express');
 const app = Express();
 const Sqlite = require('sqlite3');
 const bodyParser = require('body-parser');
-app.use(bodyParser());
+app.use(bodyParser.json());
 
 let DB = new Sqlite.Database(DB_NAME);
+
+app.get('/messages', (req, res) => {
+    sql = "SELECT * FROM MESSAGES"
+    DB.all(sql, [], (err, rows) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+        res.send(rows);
+    });
+});
 
 app.get('/messages/:id', (req, res) => {
     sql = "SELECT * FROM MESSAGES WHERE ID = " + req.params.id;
@@ -19,15 +30,20 @@ app.get('/messages/:id', (req, res) => {
 });
 
 app.post('/messages', (req, res) => {
+    console.log(req.body);
+    var author = req.body.author;
+    var content = req.body.content;
     sql = `INSERT INTO MESSAGES
-                (AUTHOR, CONTENT, DATE)
+                (AUTHOR, CONTENT)
            VALUES
-                (${req.body.author}, ${req.body.content}, ${req.body.date});
+                (?, ?);
     `;
-    DB.all(sql, [], (err, rows) => {
+    DB.run(sql, [author, content], function (err) {
         if (err) {
             console.log(err);
             res.sendStatus(500);
+        } else {
+            res.sendStatus(201);
         }
     });
 });
