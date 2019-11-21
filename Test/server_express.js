7e45b97a996761f368e55f6acd19c060c31a7c57
geiser1u <sylvain.geiser2@etu.univ-lorraine.fr>
2019-11-21 12:46:03 +0100

    Done

--- Test/server_express.js
+++ Test/server_express.js
@@ -1,7 +1,28 @@
 const Express = require('express');
 const app = Express();
+
 app.get('/hello/:name', (req, res) => {
     res.end('hello: ' + req.params.name);
+});
+
+app.get('/messages', (req, res) => {
+    res.end('Error 501');
+});
+
+app.get('/messages/:id', (req, res) => {
+    res.end('Error 501');
+});
+
+app.post('/messages', (req, res) => {
+    res.end('Error 501');
+});
+
+app.patch('/messages/:id', (req, res) => {
+    res.end('Error 501');
+});
+
+app.delete('/messages/:id', (req, res) => {
+    res.end('Error 501');
 });
 
 app.listen(3000);
